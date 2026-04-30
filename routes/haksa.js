b0c85c3e1e57ec23fe810d607ac18d557617e331
seungyeon <qwer6021@gmail.com>
2026-04-30 15:31:44 +0900

    web page 커밋

---
+++ routes/haksa.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var {getConnection} = require("./connect");
+const OracleDB = require('oracledb');
+/*교수관리 페이지 */
+router.get('/pro', function(req, res, next) {
+  res.render('index',{title:'교수관리', pageName:'haksa/professors'})
+});
+
+/*교수 데이터 생성*/
+router.get('/pro/list.json', async function(req,res){
+  var con;
+  try{
+    con = await getConnection();
+    const sql="select * from professors";
+    const result = await con.execute(sql, {}, {outFormat:OracleDB.OUT_FORMAT_OBJECT});
+    res.send(result.rows);
+  }catch(err){
+
+  }finally{
+    if(con) await con.close();
+  }
+});
+
+/*학생관리 페이지 */
+router.get('/stu', function(req, res, next) {
+  res.render('index',{title:'학생관리', pageName:'haksa/students'})
+});
+/*교수관리 페이지 */
+router.get('/cou', function(req, res, next) {
+  res.render('index',{title:'강좌관리', pageName:'haksa/courses'})
+});
+
+module.exports = router;
